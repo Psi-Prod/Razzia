6b4061d8e747a9f3a494d211c77b433e83a1178b
Tim-ats-d <tim.arnouts@protonmail.com>
2023-02-03 23:39:43 +0100

    Fix ipv6 in Razzia_unix.

--- razzia-unix/razzia_unix.ml
+++ razzia-unix/razzia_unix.ml
@@ -6,7 +6,15 @@ module Stack = Tcpip_stack_socket.V4V6
 open Lwt.Syntax
 
 let get ?(v4 = Ipaddr.V4.Prefix.global) ?v6 req =
-  let* tcp = Stack.TCP.connect ~ipv4_only:false ~ipv6_only:false v4 v6 in
-  let* udp = Stack.UDP.connect ~ipv4_only:false ~ipv6_only:false v4 v6 in
+  let* tcp =
+    Stack.TCP.connect ~ipv4_only:false
+      ~ipv6_only:(match v6 with None -> true | _ -> false)
+      v4 v6
+  in
+  let* udp =
+    Stack.UDP.connect ~ipv4_only:false
+      ~ipv6_only:(match v6 with None -> true | _ -> false)
+      v4 v6
+  in
   let* stack = Stack.connect udp tcp in
   get stack req
