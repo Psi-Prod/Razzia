3487297ffeade472522c43561beba12a8db70126
Tim-ats-d <tim.arnouts@protonmail.com>
2023-03-14 19:25:14 +0100

    Fix deps, TOFU and client cert for razzia-eio.

--- razzia-eio/razzia_eio.ml
+++ razzia-eio/razzia_eio.ml
@@ -7,9 +7,6 @@ end
 
 open Eio
 
-let tls_config =
-  Tls.Config.client ~authenticator:(fun ?ip:_ ~host:_ _certs -> Ok None) ()
-
 let ( let+ ) x f =
   match x with
   | Error (`Msg msg) -> Error (`Host (`InvalidHostname msg))
@@ -26,12 +23,16 @@ module HeaderParser = Razzia.Private.MakeParser (struct
     | c -> Some (Some c)
 end)
 
-let connect ~net (service, host) request =
+module TlsClientCfg = Razzia.Private.TlsCfg (Pclock)
+
+let connect ~net (service, host) req =
   Net.with_tcp_connect net ~service ~host (fun flow ->
       let+ dn = Domain_name.of_string host in
       let+ host = Domain_name.host dn in
-      let client = Tls_eio.client_of_flow tls_config ~host flow in
-      Flow.copy_string (Format.asprintf "%a" Razzia.pp_request request) client;
+      let client =
+        Tls_eio.client_of_flow (TlsClientCfg.make req (ref None)) ~host flow
+      in
+      Flow.copy_string (Format.asprintf "%a" Razzia.pp_request req) client;
       let buf = Buf_read.of_flow client ~max_size:Sys.max_string_length in
       try
         match HeaderParser.parse buf with
